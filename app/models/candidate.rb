8efcd3029990103fbf63aa75a783b5a222a818b4
Ian Boyle <irboyle21@gmail.com>
2022-02-19 14:14:47 -0500

    adds relationships to models

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,5 @@
 class Candidate < ApplicationRecord
+  belongs_to :user
+  has_many :opportunity_candidates
+  has_many :opportunities, through: :opportunity_candidates
 end
--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -1,2 +1,4 @@
 class Recruiter < ApplicationRecord
+  belongs_to :user
+  has_many :opportunities
 end
