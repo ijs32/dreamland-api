fa37f490632cbe5b44582e680fc4785f22c509eb
Ian S <iansilber32@gmail.com>
2022-02-19 19:20:00 -0500

    minor name fix

--- test/controllers/candidates_controller_test.rb
+++ test/controllers/candidates_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class CandidateControllerTest < ActionDispatch::IntegrationTest
+class CandidatesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
