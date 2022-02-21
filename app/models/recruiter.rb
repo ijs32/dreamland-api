ccdd0337ce25725d2fe485ffcf6af1e0d2d88692
Ian Boyle <irboyle21@gmail.com>
2022-02-21 10:26:39 -0500

    adds get_matched_opportunities method to recruiter model. recruiters index now renders all opportunities that have a matched candidate.. currently shows all offers. need to show only offers that pertain to the logged in recruiter.

--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -6,4 +6,9 @@ class Recruiter < ApplicationRecord
     candidate = Candidate.find_by(id: candidate_id)
     return candidate
   end
+
+  def get_matched_opportunities
+    matched_opportunities = opportunities.map{|opp| Offer.find_by(opportunity_id: opp.id)}
+    return matched_opportunities
+  end
 end
